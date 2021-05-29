dfcedc78fed336540c985f0a3492d75afe2ce0a2
OnlyOneUseAcc <61562916+OnlyOneUseAcc@users.noreply.github.com>
2021-05-29 16:37:58 +0300

    update get request

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ app = FastAPI()
 
 @app.get("/user_recomendation")
 async def predict(user_id: int):
-    return {'content': {'films': {1: 'film1',
-                                  2: 'film2',
-                                  3: 'film3'},
-                        'serials': {1: 'serial1',
-                                    2: 'serial2',
-                                    3: 'serial3'}
+    return {'content': {'films': {1: {'name': 'film1',
+                                      'genre': 'genre1, genre2'},
+                                  2: {'name': 'film2',
+                                      'genre': 'genre1, genre2'},
+                                  3: {'name': 'film3',
+                                      'genre': 'genre1, genre2'}},
+                        'serials': {1: {'name': 'serial1',
+                                        'genre': 'genre1, genre2'},
+                                    2: {'name': 'serial2',
+                                        'genre': 'genre1, genre2'},
+                                    3: {'name': 'serial3',
+                                        'genre': 'genre1, genre2'}}
                         },
             'user_id': user_id
             }
