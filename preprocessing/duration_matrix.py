16a831bc43193e0cef006d68035e9ec240fdf9a2
paNoNi <novgorodcevnp@gmail.com>
2021-05-29 08:42:42 +0300

    add sparse matrix

---
+++ preprocessing/duration_matrix.py
@@ -0,0 +1,59 @@
+from scipy import sparse
+import numpy as np
+import pandas as pd
+
+CONST_PATH = '../data/postprocessing/'
+CONTENT_FILE = 'content.csv'
+HISTORY_FILE = 'watch_history.csv'
+NAME_MATRIX = 'sparse_matrix.npz'
+
+
+class DurationSparseMatrix:
+    def __init__(self):
+        self.__tables = {'users': dict(),
+                         'contents': dict()}
+
+    def __read_table(self, name: str):
+        index_col = 0 if name == HISTORY_FILE else 'content_uid'
+        return pd.read_csv(f'{CONST_PATH}{name}', index_col=index_col)
+
+    def read_info(self):
+        history = self.__read_table(HISTORY_FILE)
+        self.__tables['users'] = {key: value for key, value in
+                                  zip(history.user_uid.unique(), range(len(history.user_uid.unique())))}
+
+        content_df = self.__read_table(CONTENT_FILE)
+        self.__tables['contents'] = {key: value for key, value in
+                                     zip(content_df.index.values, range(len(content_df.index.values)))}
+
+        return history, content_df
+
+    def create_empty_matrix(self, shape: tuple):
+        view_matrix = sparse.lil_matrix(np.zeros(shape))
+        self.save_matrix(view_matrix)
+
+    def save_matrix(self, matrix):
+        sparse.save_npz(f'{CONST_PATH}{NAME_MATRIX}', matrix.tocsr())
+
+    def load_matrix(self):
+        return sparse.load_npz(f'{CONST_PATH}{NAME_MATRIX}').tolil()
+
+    def fill_matrix(self):
+        history, content_df = self.read_info()
+        view_matrix = self.load_matrix()
+        for _, row in history.iterrows():
+            view_matrix[self.__tables['users'][row.user_uid], self.__tables['contents'][row.content_uid]] = \
+                row.second / content_df.loc[row.content_uid, 'duration_seconds']
+        self.save_matrix(view_matrix)
+
+    def get_matrix_part(self, ids: tuple, ax_type='users'):
+        ids = tuple(ids)
+        matrix_part = [self.__tables[ax_type][user] for user in ids]
+        view_matrix = self.load_matrix()
+        lines = view_matrix[matrix_part] if ax_type == 'users' else view_matrix[:, matrix_part]
+        return lines.toarray()
+
+    def get_shape(self):
+        len_users = len(self.__tables['users'])
+        len_contents = len(self.__tables['contents'])
+        return len_users, len_contents
